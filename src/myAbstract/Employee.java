20ace2f748b2245c4c74745ffff3c17bfad2ba96
Tammie <Tammie@10.10.10.104>
2014-01-26 17:33:50 -0600

    added properties

--- src/myAbstract/Employee.java
+++ src/myAbstract/Employee.java
@@ -1,15 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package myAbstract;
 
-/**
- *
- * @author Tammie
- */
+
 public abstract class Employee {
+    private static int employeeSSN;
+    private static String hireDate;
+    private static int employeeID;
     
 }
