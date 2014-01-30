29ca8614fc81403f367f4a2adbab54be1ad7fa00
Tammie <Tammie@10.30.19.80>
2014-01-29 21:35:25 -0600

    fixed most of interface issues

--- src/myInterface/SalaryPlusComissionEmployee.java
+++ src/myInterface/SalaryPlusComissionEmployee.java
@@ -2,23 +2,29 @@ package myInterface;
 
 public class SalaryPlusComissionEmployee extends SalariedEmployee {
 
-    private double salary;
-    private int employeeSSN;
+    private double employeePay;
+    private double employeeSSN;
     private String hireDate;
     private String firstName;
     private String lastName;
-
+    private double employeeID;
     private double commisionRate;
     private double salaryPlusCommisionRate;
 
-    SalaryPlusComissionEmployee(String firstName, String lastName, int employeeSSN,
-            String hireDate, double salary) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public SalaryPlusComissionEmployee(double employeePay, double employeeSSN, String hireDate, String firstName, String lastName, double employeeID, double salaryPlusCommisionRate, String firstName, String lastName, int employeeSSN, String hireDate, double employeePay) {
+        super(firstName, lastName, employeeSSN, hireDate, employeePay);
+        this.employeePay = employeePay;
         this.employeeSSN = employeeSSN;
         this.hireDate = hireDate;
-        this.salary = salary;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employeeID = employeeID;
+        this.salaryPlusCommisionRate = salaryPlusCommisionRate;
     }
+
+
+    
+    
 
     
 
@@ -38,5 +44,6 @@ public class SalaryPlusComissionEmployee extends SalariedEmployee {
     public void setSalaryPlusCommisionRate(double salaryPlusCommisionRate) {
         this.salaryPlusCommisionRate = salaryPlusCommisionRate;
     }
+    
 
 }
