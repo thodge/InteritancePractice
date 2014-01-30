216d8f9db4c01604e72b89d26a37795ab1e0aae2
Tammie <Tammie@10.30.19.80>
2014-01-29 20:43:31 -0600

    fixed some float/int/double issues in interface

--- src/myInterface/Employee.java
+++ src/myInterface/Employee.java
@@ -2,16 +2,11 @@ package myInterface;
 
 public interface Employee {
 
-    public void employeeSSN();
-
-    public void hireDate();
-
+    public abstract void employeeSSN();
+    public abstract void hireDate();
     public abstract void firstName();
-
     public abstract void lastName();
-
     public abstract double getEmployeePay();
-
     public abstract double employeeID();
 
 }
