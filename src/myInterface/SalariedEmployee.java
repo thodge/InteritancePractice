4d50c396b71c3c34e9e153cb7711733bbe160964
Tammie <Tammie@10.30.19.80>
2014-01-29 20:45:46 -0600

    fixed some float/int/double issues in interface

--- src/myInterface/SalariedEmployee.java
+++ src/myInterface/SalariedEmployee.java
@@ -2,20 +2,60 @@ package myInterface;
 
 public class SalariedEmployee implements Employee {
 
-    private int hourlyPayRate;
+    private double salary;
     private int employeeSSN;
     private String hireDate;
     private String firstName;
     private String lastName;
 
     SalariedEmployee(String firstName, String lastName, int employeeSSN,
-            String hireDate) {
+            String hireDate, double salary) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.employeeSSN = employeeSSN;
         this.hireDate = hireDate;
+        this.salary = salary;
     }
 
-        
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getEmployeeSSN() {
+        return employeeSSN;
+    }
+
+    public void setEmployeeSSN(int employeeSSN) {
+        this.employeeSSN = employeeSSN;
+    }
+
+    public String getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(String hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
 
 }
